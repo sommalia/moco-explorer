c1034ecf7acd7362a6962ea2a519b33b28e7cfa9
sommalia <sommalia@tuta.io>
2020-03-25 13:32:39 +0100

    implement json formatting

--- moco_explorer/models/format/json.py
+++ moco_explorer/models/format/json.py
@@ -1,5 +1,16 @@
 from .base import BaseFormatter
+from json import dumps
+from click import echo
 
 
 class JsonFormatter(BaseFormatter):
-    pass
+
+    def format_single(self, target):
+        echo(
+            dumps(target, indent=4, sort_keys=True, default=lambda x: x.__dict__)
+        )
+
+    def format_list(self, target):
+        echo(
+            dumps(target, indent=4, sort_keys=True, default=lambda x: x.__dict__)
+        )
