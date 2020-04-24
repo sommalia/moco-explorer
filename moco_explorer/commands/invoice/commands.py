ee8bced95aaccf27943710baa4c08108928da884
sommalia <sommalia@tuta.io>
2020-04-24 12:27:12 +0200

    rework invoices

--- moco_explorer/commands/invoice/commands.py
+++ moco_explorer/commands/invoice/commands.py
@@ -1,5 +1,5 @@
 import click
-
+from os.path import realpath
 
 @click.group()
 @click.pass_context
@@ -7,21 +7,34 @@ def invoice(ctx):
     pass
 
 
-@invoice.command()
+@invoice.command(help="Retrieve a single invoice")
 @click.option("-p", "--page", help="page number", type=int, default=1)
 @click.option('-a', "--retrieve-all", help="Retrieve all objects from the system (ignores page option)", is_flag=True)
 @click.pass_context
 def getlist(ctx, page, retrieve_all):
+    """
+    Retrieve a list of invoice objects
+
+    :param ctx: click context
+    :param page: page number
+    :param retrieve_all: flag to retrieve a objects from the system (ignores page option)
+    :return: formatted list of invoice objects
+    """
     formatter = ctx.obj["format"]
     moco = ctx.obj["moco"]
 
     if retrieve_all:
         all_items = []
-        items_list = moco.Invoice.getlist(page=1)
-        all_items.extend(items_list.items)
 
-        for i in range(2, items_list.last_page + 1):
-            items_list = moco.Invoice.getlist(page=i)
+        end_reached = False
+        next_page = 1
+
+        while not end_reached:
+            items_list = moco.Invoice.getlist(page=next_page)
+
+            next_page = items_list.next_page
+            end_reached = items_list.is_last
+
             all_items.extend(items_list.items)
 
         formatter.format_list(all_items)
@@ -30,10 +43,18 @@ def getlist(ctx, page, retrieve_all):
         formatter.format_list(items_list.items)
 
 
-@invoice.command()
+@invoice.command(help="Retrieve a single invoice")
 @click.argument("invoice-id", type=int)
 @click.pass_context
 def get(ctx, invoice_id):
+    """
+    Retrieve a single invoice
+
+    :param ctx: click context
+    :param invoice_id: id of the invoice
+    :return: formatted invoice object
+    """
+
     formatter = ctx.obj["format"]
     moco = ctx.obj["moco"]
 
@@ -41,12 +62,22 @@ def get(ctx, invoice_id):
     formatter.format_single(invoice_item.data)
 
 
-@invoice.command()
+@invoice.command(help="Retrieve the pdf document of an invoice")
 @click.argument("invoice-id", type=int)
-@click.argument("out_path", type=click.Path(exists=False, writable=True))
+@click.argument("out-path", type=click.Path(exists=False, writable=True))
 @click.pass_context
 def pdf(ctx, invoice_id, out_path):
+    """
+    Retrieve the pdf document of an invoice
+
+    :param ctx: click context
+    :param invoice_id: id of the invoice
+    :param out_path: path for the pdf file to saved to
+    """
+
     moco = ctx.obj["moco"]
 
     invoice_pdf = moco.Invoice.pdf(invoice_id)
     invoice_pdf.write_to_file(out_path)
+
+    click.echo("Pdf document successfully saved to {}".format(realpath(out_path)))
